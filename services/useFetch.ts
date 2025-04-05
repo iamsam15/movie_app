b504e9ac05f37adf85b8eacc4e9f0180e3cf82d0
iamsam15 <134360554+iamsam15@users.noreply.github.com>
2025-04-05 12:00:46 +0530

    feat: home page

---
+++ services/useFetch.ts
@@ -0,0 +1,42 @@
+//fetchMovies
+//fetchMovieDetails
+//useFetch(fetchMovies)
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const results = await fetchFunction();
+
+      setData(results);
+    } catch (error) {
+      //@ts-ignore
+      setError(error instanceof Error ? error : new Error("An error occured"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, Refetch: fetchData, reset };
+};
+
+export default useFetch;
